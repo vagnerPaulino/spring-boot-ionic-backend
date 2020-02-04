764db0def8b28fbdeb6aa5296781d50762a48dca
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-02-03 22:33:15 -0200

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/pvagner/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.pvagner.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.pvagner.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
