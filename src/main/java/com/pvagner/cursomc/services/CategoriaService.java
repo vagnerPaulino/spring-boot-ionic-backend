d4e63c7e11f8708cbfa829a96f9f35c72cc5c82a
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-02-07 20:51:07 -0200

    Validação sintatica com Bean Validation

--- src/main/java/com/pvagner/cursomc/services/CategoriaService.java
+++ src/main/java/com/pvagner/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.pvagner.cursomc.domain.Categoria;
+import com.pvagner.cursomc.dto.CategoriaDTO;
 import com.pvagner.cursomc.repositories.CategoriaRepository;
 import com.pvagner.cursomc.services.exceptions.DataIntegrityException;
 import com.pvagner.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
