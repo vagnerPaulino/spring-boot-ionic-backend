6efec289dfd126682efc34f36362c4f006449454
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-02-13 17:07:36 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/pvagner/cursomc/config/DevConfig.java
+++ src/main/java/com/pvagner/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.pvagner.cursomc.services.DBService;
+import com.pvagner.cursomc.services.EmailService;
+import com.pvagner.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 		}
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
