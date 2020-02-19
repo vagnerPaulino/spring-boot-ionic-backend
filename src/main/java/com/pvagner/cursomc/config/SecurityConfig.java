b8bac0478a6b2a43b8ecb79491b6421eb748d7bb
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-02-19 20:48:12 -0300

    Implementando autorização

--- src/main/java/com/pvagner/cursomc/config/SecurityConfig.java
+++ src/main/java/com/pvagner/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.pvagner.cursomc.security.JWTAuthenticationFilter;
+import com.pvagner.cursomc.security.JWTAuthorizationFilter;
 import com.pvagner.cursomc.security.JWTUtil;
 
 @Configuration
@@ -56,6 +57,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHES).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
