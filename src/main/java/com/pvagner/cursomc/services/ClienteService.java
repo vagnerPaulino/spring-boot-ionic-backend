264d061916b7d145ebd422e8a17412dcceed2c11
vagnerPaulino <vagnerpaulinop@gmail.com>
2020-02-20 22:16:26 -0300

    Restrição de conteúdo: cliente só recupera ele mesmo

--- src/main/java/com/pvagner/cursomc/services/ClienteService.java
+++ src/main/java/com/pvagner/cursomc/services/ClienteService.java
@@ -15,11 +15,14 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pvagner.cursomc.domain.Cidade;
 import com.pvagner.cursomc.domain.Cliente;
 import com.pvagner.cursomc.domain.Endereco;
+import com.pvagner.cursomc.domain.enums.Perfil;
 import com.pvagner.cursomc.domain.enums.TipoCliente;
 import com.pvagner.cursomc.dto.ClienteDTO;
 import com.pvagner.cursomc.dto.ClienteNewDTO;
 import com.pvagner.cursomc.repositories.ClienteRepository;
 import com.pvagner.cursomc.repositories.EnderecoRepository;
+import com.pvagner.cursomc.security.UserSS;
+import com.pvagner.cursomc.services.exceptions.AuthorizationException;
 import com.pvagner.cursomc.services.exceptions.DataIntegrityException;
 import com.pvagner.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -36,6 +39,12 @@ public class ClienteService {
 	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
+		
+		UserSS user = UserService.authenticated();
+		if (user==null || !user.hasRole(Perfil.ADMIN) && !id.equals(user.getId())){
+			throw new AuthorizationException("Acesso negado");
+		}
+		
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
